ba0799f1e492f5edacafc403f6e89487d67eab58
visualstudio <visualstudio@122-PC32>
2019-02-06 11:11:14 -0500

    peanut birthday

--- c0732442/Program.cs
+++ c0732442/Program.cs
@@ -10,6 +10,43 @@ namespace c0732442
     {
         static void Main(string[] args)
         {
+
+
+        }
+        
+    }
+
+    class Dog
+    {
+        public static int NumberofDogs = 0;
+        public Dog()
+        {
+            Dog.NumberofDogs++;
+        }
+        public String DogName;
+        public String DogBreed;
+        public Dog nextDog;
+     }
+    class LinkedList
+    {
+        public Dog Head;
+        public Dog Peanut = new Dog();
+        public Dog Fifi = new Dog();
+        public Dog Jordan = new Dog();
+        public Dog Fido = new Dog();
+
+
+        public void run()
+        {
+            Head = Peanut;
+            Peanut.DogName = "Peanut";
+            Peanut.DogBreed = "Bichon";
+            Fifi.DogName = "Fifi";
+            Fifi.DogBreed = "Poodle";
+            Jordan.DogName = "Jodan";
+            Jordan.DogBreed = "German shepard";
+            Fido.DogName = "Fido";
+            Fido.DogBreed = "beegle";
         }
     }
 }
